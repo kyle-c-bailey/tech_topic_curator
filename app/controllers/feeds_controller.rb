07a0ac3f8b18c90882d7f89508a736401b5d9bcf
Kyle Bailey <kyleb@spiceworks.com>
2017-03-23 11:22:32 -0500

    First version on word aggregator algorithm

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,28 +1,22 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
-  # GET /feeds
-  # GET /feeds.json
+  COMMON_WORDS = ['a', 'about', 'all', 'also', 'and', 'are', 'as', 'at', 'be', 'because', 'but', 'by', 'can', 'come', 'could', 'day', 'do', 'even', 'find', 'first', 'for', 'from', 'get', 'give', 'go', 'have', 'he', 'her', 'here', 'him', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'know', 'like', 'look', 'make', 'man', 'many', 'me', 'more', 'my', 'new', 'no', 'not', 'now', 'of', 'on', 'one', 'only', 'or', 'other', 'our', 'out', 'people', 'say', 'see', 'she', 'so', 'some', 'take', 'tell', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'thing', 'think', 'this', 'those', 'time', 'to', 'two', 'up', 'use', 'very', 'want', 'way', 'we', 'well', 'what', 'when', 'which', 'who', 'will', 'with', 'would', 'year', 'you', 'your', '-', ':']
+
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1
-  # GET /feeds/1.json
   def show
   end
 
-  # GET /feeds/new
   def new
     @feed = Feed.new
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds
-  # POST /feeds.json
   def create
     @feed = Feed.new(feed_params)
 
@@ -37,8 +31,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /feeds/1
-  # PATCH/PUT /feeds/1.json
   def update
     respond_to do |format|
       if @feed.update(feed_params)
@@ -51,8 +43,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1
-  # DELETE /feeds/1.json
   def destroy
     @feed.destroy
     respond_to do |format|
@@ -61,13 +51,31 @@ class FeedsController < ApplicationController
     end
   end
 
+  def word_aggregator
+    word_hash = Hash.new
+    entry_titles = Entry.pluck(:title)
+
+    Entry.all.each do |entry|
+      entry.title.split.each do |word|
+        next if COMMON_WORDS.include?(word.downcase)
+        if word_hash.has_key?(word)
+          next if word_hash[word][:sources].include?(entry.url)
+          word_hash[word][:count] = word_hash[word][:count] + 1
+          word_hash[word][:sources] << entry.url
+        else
+          word_hash[word] = {count: 1, sources: [entry.url]}
+        end
+      end
+    end
+
+    @word_frequencies = word_hash.sort_by{|word, meta| meta[:count]}
+  end
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_feed
       @feed = Feed.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
       params.require(:feed).permit(:name, :url, :description)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :feeds do
+    collection do
+      get 'word_aggregator'
+    end
     member do
      resources :entries, only: [:index, :show]
     end
