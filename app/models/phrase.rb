e0663f8fa8784f63626345ed0074f29688f379c1
Kyle Bailey <kyleb@spiceworks.com>
2017-03-27 13:41:15 -0500

    Add clean_title to remove common article prefixes

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -2,14 +2,11 @@ class Phrase < ApplicationRecord
   has_many :phrase_entries, dependent: :destroy
   has_many :entries, through: :phrase_entries
 
-  BLACKLIST = ["WATCH"]
-
   def count
     self.phrase_entries.count
   end
 
   def self.create_or_increment(content, entry_id)
-    return if BLACKLIST.include?(content)
     phrase = Phrase.where(content: content).take
     unless phrase.present?
       phrase = Phrase.create!(content: content)
