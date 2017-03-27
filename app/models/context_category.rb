38f8be15a038f0da333ceebcdd86b3c17cdb7ed9
Kyle Bailey <kyleb@spiceworks.com>
2017-03-27 09:22:29 -0500

    Add entry context association to context category

--- app/models/context_category.rb
+++ app/models/context_category.rb
@@ -1,3 +1,5 @@
 class ContextCategory < ApplicationRecord
   has_many :context_words, dependent: :destroy
+  has_many :entry_contexts, dependent: :destroy
+  has_many :entries, through: :entry_contexts
 end
